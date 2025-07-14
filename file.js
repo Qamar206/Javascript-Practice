ba6e53fe84fa14af417b597bc57e142a305a471d
Qamar206 <mygsmcode786@gmail.com>
2025-07-14 18:36:52 +0500

    working html and creating file.js
    working html and creating file.js

--- file.js
+++ file.js
@@ -1,22 +1,24 @@
-{fullname = "Qamar Ali";
-age=34
-price=100
-console.log(fullname)
-console.log(age)
-console.log(price)
+// {fullname = "Qamar Ali";
+// age=34
+// price=100
+// console.log(fullname)
+// console.log(age)
+// console.log(price)
 
-Example = "Example"
-fullName = "fullName";
-fullname = "fullname";
-console.log  (Example)
-console.log (fullName)
-console.log (fullname)
-}
-{
-    let fullName = "Qamar Ali"
+// Example = "Example"
+// fullName = "fullName";
+// fullname = "fullname";
+// console.log  (Example)
+// console.log (fullName)
+// console.log (fullname)
+// }
+// {
+//     let fullName = "Qamar Ali"
+// let age = 34;
+// let totalPrice = 1000;
+// console.log (fullName)
+// console.log (age)
+// console.log (totalPrice)
+// }
+
 let age = 34;
-let totalPrice = 1000;
-console.log (fullName)
-console.log (age)
-console.log (totalPrice)
-}
