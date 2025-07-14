44eb485cfcc5157614ea6dd3758ab9143c46b84a
Qamar206 <mygsmcode786@gmail.com>
2025-07-14 16:51:56 +0500

    working on java script

--- file.js
+++ file.js
@@ -4,3 +4,10 @@ price=100
 console.log(fullname)
 console.log(age)
 console.log(price)
+
+Example = "Example"
+fullName = "fullName";
+fullname = "fullname";
+console.log  (Example)
+console.log (fullName)
+console.log (fullname)
