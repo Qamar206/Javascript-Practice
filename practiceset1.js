5a7b457b8abea0fb7aac17366edc2b142267f010
Qamar206 <mygsmcode786@gmail.com>
2025-07-20 00:26:49 +0500

    practiceset 1.js working
    practiceset 1.js working

--- practiceset1.js
+++ practiceset1.js
@@ -5,3 +5,16 @@ const product ={
     price: 300,
 };
 console.log(product)
+
+const profile = {
+    username: "Qamar Ali",
+    isFollow: false,
+    followers: 123,
+    following: 123,
+}
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile["username"])
+console.log(typeof profile["isFollow"])
+console.log(typeof profile["followers"])
+console.log(typeof profile["following"])
