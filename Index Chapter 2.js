78fd76994455484448a70570070c82e1b80e29b6
Qamar206 <mygsmcode786@gmail.com>
2025-07-20 04:56:32 +0500

    practice set 2

--- Index Chapter 2.js
+++ Index Chapter 2.js
@@ -32,8 +32,18 @@ console.log("Hello World");
 let a = 5;
 let b = 2;
 
-a += 4
-console.log (a)
+// a += 4
+// console.log ("a =",a)
 
-a -= 4
-console.log (a)
+// a -= 4
+// console.log ("a =",a)
+
+// a *= 4
+// console.log ("a =",a)
+
+// a %= 4
+// console.log ("a =",a)
+
+// a **= 4
+// console.log ("a =",a)
+
