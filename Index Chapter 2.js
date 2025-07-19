663b4123253d3e966991d160ef5c583c501d3419
Qamar206 <mygsmcode786@gmail.com>
2025-07-20 04:30:14 +0500

    javascript practice set 2

--- Index Chapter 2.js
+++ Index Chapter 2.js
@@ -1,29 +1,39 @@
 console.log("Hello World");
 
-//Arithmetic Operator
-let a = 5;
-let b = 2;
+// //Arithmetic Operator
+// let a = 5;
+// let b = 2;
 
-console.log ("a =", a)
-console.log ("b =", b)
-//or
-console.log (" a =", a, "& b =", b)
-console.log (" a + b = ", a + b)
-console.log (" a - b = ", a - b)
-console.log (" a * b = ", a * b)
-console.log (" a / b = ", a / b)
-console.log (" a % b = ", a % b)
-console.log (" a ** b = ", a**b) // 5^2 = 5x5 = 25
-console.log ("a++ = ", a++) // Unary Operator
-console.log (a++) //6
-console.log ("a++ = ", a++) //7
+// console.log ("a =", a)
+// console.log ("b =", b)
+// //or
+// console.log (" a =", a, "& b =", b)
+// console.log (" a + b = ", a + b)
+// console.log (" a - b = ", a - b)
+// console.log (" a * b = ", a * b)
+// console.log (" a / b = ", a / b)
+// console.log (" a % b = ", a % b)
+// console.log (" a ** b = ", a**b) // 5^2 = 5x5 = 25
+// console.log ("a++ = ", a++) // Unary Operator, Value is 5 it will change from next line.
+// console.log (a++) //6
+// console.log ("a++ = ", a++) //7
 
 
-// Unary Operator
+//Unary Operator
 // let a = 5;
 // let b = 2;
 
 // a = a + 1;
-// console.log (a)
+// console.log (a) //6
 // a++
-// console.log (a)
+// console.log (a) //7
+
+// Assignment Operators
+let a = 5;
+let b = 2;
+
+a += 4
+console.log (a)
+
+a -= 4
+console.log (a)
