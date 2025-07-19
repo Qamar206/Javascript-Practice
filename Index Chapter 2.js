ea6899517098df5ac66a6a1825f1c5c41c622a24
Qamar206 <mygsmcode786@gmail.com>
2025-07-20 04:14:15 +0500

    working on practice set of javascript and learning operators
    working on practice set of javascript and learning operators

--- Index Chapter 2.js
+++ Index Chapter 2.js
@@ -13,4 +13,17 @@ console.log (" a - b = ", a - b)
 console.log (" a * b = ", a * b)
 console.log (" a / b = ", a / b)
 console.log (" a % b = ", a % b)
+console.log (" a ** b = ", a**b) // 5^2 = 5x5 = 25
+console.log ("a++ = ", a++) // Unary Operator
+console.log (a++) //6
+console.log ("a++ = ", a++) //7
 
+
+// Unary Operator
+// let a = 5;
+// let b = 2;
+
+// a = a + 1;
+// console.log (a)
+// a++
+// console.log (a)
