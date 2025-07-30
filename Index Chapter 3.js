6fe4938389d497ca705d0a21974a20ec5e415938
Qamar206 <mygsmcode786@gmail.com>
2025-07-30 15:02:39 +0500

    chapter 3
    Chapter 3

--- Index Chapter 3.js
+++ Index Chapter 3.js
@@ -2,5 +2,13 @@ console.log ("Index Chapter 3")
 
 //this is a for loop example
 for (let i=1; i<=5; i++){   
- console.log ("index chapter 3");
+ console.log ("Index Ihapter 3");
 } //this is a for loop example
+
+//Calculate sum o 1 to 5 Numbers
+let sum = 0;
+for (i=1;i<=5;i++){
+    sum = sum + i;
+}
+console.log ("sum =", sum)
+console.log ("Loop Has Ended")
