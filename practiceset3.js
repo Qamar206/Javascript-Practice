541f2d0acce6760eb8e563ac819069897462a3ca
Qamar206 <mygsmcode786@gmail.com>
2025-07-31 17:27:35 +0500

    chapter 3

---
+++ practiceset3.js
@@ -0,0 +1,10 @@
+for (let num = 0; num <= 100; num++){
+if (num%2 === 0){ //
+    console.log ("num=", num)
+} else {
+
+}
+}
+
+console.log ("usage of for loop")
+console.log ("---------------------------")
