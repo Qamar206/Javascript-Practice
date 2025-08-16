ed9057a1bff45293aa7a3748878f8560704191d0
Qamar206 <mygsmcode786@gmail.com>
2025-08-16 18:30:20 +0500

    working on chapter 3

--- practiceset3.js
+++ practiceset3.js
@@ -1,3 +1,4 @@
+//Practice Set Question 1 Print The Even Numbers On Coonsole
 for (let num = 0; num <= 100; num++){
 if (num%2 === 0){ //
     console.log ("num=", num)
@@ -6,5 +7,20 @@ if (num%2 === 0){ //
 }
 }
 
-console.log ("usage of for loop")
+console.log ("usage of for loop printing the even numbers till 100")
 console.log ("---------------------------")
+
+//Practice Question Print The Odd Numbers
+for (let num1 = 0; num1 <= 100; num1++){
+    if (num1%2 !==0){
+        console.log ("num=",num1)
+    }else {
+
+    }
+}
+console.log ("usage of for loop printing the odd numbers till 100")
+
+
+//Practice Question 2 
+// Create a game where you start with any random game number. Ask the user to keep guessing the game number
+// until the user enters correct value.
